7505170059b12f8d82acc30c9d967e8a5ead5a2d
gitwithashish <ashishyadav27062@gmail.com>
2026-06-07 22:14:54 +0530

    solved all problem for day 7

---
+++ Day7/25_question.cpp
@@ -0,0 +1,21 @@
+//Write a program to Recursive factorial.
+#include <iostream>
+using namespace std;
+
+int factorial(int x)
+{
+    if (abs(x) < 2)
+        return 1;
+    return x * factorial(x - 1);
+}
+int main()
+{
+    int n;
+    cout << "enter the number whose factorial is to be calculated: ";
+    cin >> n;
+
+    int fact = factorial(n);
+
+    cout << fact << endl;
+    return 0;
+}
